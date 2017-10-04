6af95fa4d54bfb6e9df56140f73671130005641e
kkdshka <kkdshka@kkdshka>
2017-10-04 16:07:22 +0300

    Changed adding tasks in settings.

--- public_html/src/settingsScript.js
+++ public_html/src/settingsScript.js
@@ -8,11 +8,32 @@ window.onload = () => {
     const schedule = new Schedule(repository);
 
     renderSchedule(week, schedule);
-}
+    const addButtons = [...document.querySelectorAll('input.add-task')];
+
+    addButtons.map((node) => {
+        node.onclick = (event) => {
+            addField(node.parentNode, node);
+            event.preventDefault();
+        };
+    });
+    
+    const deleteButtons = [...document.querySelectorAll('input.delete-task')];
+    
+    deleteButtons.map((node) => {
+        node.onclick = (event) => {
+            node.parentNode.remove();
+            event.preventDefault();
+        };
+    });
+};
 
 function renderSchedule(week, schedule) {
     week.days.map((day) => {
-        document.getElementById(day).value = schedule.schedule[day].map((value) => value.name).join(", ");
+        const parent = document.getElementById(day);
+        const add = parent.getElementsByClassName("add-task")[0];
+        schedule.schedule[day].map((value) => {
+            addField(parent, add, value.name);
+        });
     });
 }
 
@@ -25,17 +46,44 @@ document.getElementById('save').onclick = () => {
 };
 
 function saveSchedule(week, schedule) {
-    let id = 0;
     const scheduleToSave = {};
+    let id = 0;
     week.days.map((day) => {
-        let dayly = [];
-        let tasks = document.getElementById(day).value.split(", ");
-        if (tasks.length > 0 && tasks[0].length > 0) {
-            tasks.map((task) => {
-                dayly.push({name: task, done: false, id: ++id});
-            });
-        }
+        const dayly = [];
+        const tasks = [...document.querySelectorAll(`#${day} .task`)];
+        tasks.map((task) => {
+            dayly.push({name: task.value, done: false, id: ++id});
+        });
         scheduleToSave[day] = dayly;
     });
-    schedule.saveSchedule();
+    schedule.saveSchedule(scheduleToSave);
+}
+
+function addField(parentElement, referenceElement, value = '') {
+    const box = document.createElement('div');
+    box.className = 'field-box';
+    parentElement.insertBefore(box, referenceElement);
+    
+    const field = createField(value);
+    box.appendChild(field);
+    
+    const del = createDeleteButton();
+    box.appendChild(del);
+}
+
+function createField(value = '') {
+    const field = document.createElement('input');
+    field.type = 'text';
+    field.value = value;
+    field.className = 'task';
+    return field;
+}
+
+function createDeleteButton() {
+    const button = document.createElement('input');
+    button.type = 'image';
+    button.alt = 'Del';
+    button.src = '../img/delete.png';
+    button.className = 'delete-task';
+    return button;
 }
